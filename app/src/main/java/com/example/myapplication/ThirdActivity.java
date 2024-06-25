3ee80def5a82735c4d992c527f02b1f8e17619c3
qwer1221 <t4475434@gmail.com>
2024-06-25 10:27:54 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        View view = findViewById(R.id.view);
+        Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        view.startAnimation(rotateAnimation);
+    }
+}
